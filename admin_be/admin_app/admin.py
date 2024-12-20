01ab3e3ab9e2e00dc92dd731ec880fe133401e06
Abid Nawaz <abidpart2ics@gmail.com>
2024-12-20 16:23:52 +0500

    fixed: password hashing system for new created users

--- admin_be/admin_app/admin.py
+++ admin_be/admin_app/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from .models import *
 from auth_user.models import User
+from django.contrib.auth.admin import UserAdmin
 
 admin.site.register(LatestCustomers)
 admin.site.register(StudentDetailModel)
 admin.site.register(StudentRecord)
 admin.site.register(Furniture)
 admin.site.register(HostelView)
-admin.site.register(User)
+admin.site.register(User,UserAdmin)
